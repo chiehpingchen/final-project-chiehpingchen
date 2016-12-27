7b3f0290d209c499a124a050e20066794876b17b
chiehpingchen <chenc312@newschool.edu>
2016-12-27 18:40:33 -0500

    3rd comit

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // config.js
 export default {
   site: {
-    title: 'React Universal Blog'
+    title: 'CheJSfinalBlog'
   },
   bucket: {
-    slug: process.env.COSMIC_BUCKET || 'react-universal-blog',
+    slug: process.env.COSMIC_BUCKET || 'chejsfinalblog',
     media_url: 'https://cosmicjs.com/uploads',
-    read_key: process.env.COSMIC_READ_KEY || '',
-    write_key: process.env.COSMIC_WRITE_KEY || ''
+    read_key: process.env.COSMIC_READ_KEY || 'hiixY1LsPdUrKNoyduuO3XRL5LAQFiDdhHfZoEtkgvO7hpvHVt',
+    write_key: process.env.COSMIC_WRITE_KEY || 'YBZuvHgQk8I6UQF2RT5yQqHsNEoTzMJy8YDZVQbCfKW6jo9vta'
   },
 }
