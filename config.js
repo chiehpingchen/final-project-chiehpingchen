21ecff2bdfe6121caf53a04e3f3eebfab7ad242d
chiehpingchen <chenc312@newschool.edu>
2016-12-27 22:46:04 -0500

    rmKEY

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ export default {
   bucket: {
     slug: process.env.COSMIC_BUCKET || 'chejsfinalblog',
     media_url: 'https://cosmicjs.com/uploads',
-    read_key: process.env.COSMIC_READ_KEY || 'WkvbJQs6z0nvA30OjVs25cUXRM0e8PcChkbuZYsn7uRh536LMF',
-    write_key: process.env.COSMIC_WRITE_KEY || 'Z8pbSrpAJAsRGDfiDBq1r2QWPWo9XYC5iwVqIvpTkLIBPHLwSg'
+    read_key: process.env.COSMIC_READ_KEY || '',
+    write_key: process.env.COSMIC_WRITE_KEY || ''
   },
 }
 
