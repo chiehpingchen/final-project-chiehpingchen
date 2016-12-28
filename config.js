340510b0be6943a80f56c7deed004ac7b167ed04
chiehpingchen <chenc312@newschool.edu>
2016-12-27 22:23:27 -0500

    configAddKey

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
-// // config.js
-// export default {
-//   site: {
-//     title: 'CheJSfinalBlog'
-//   },
-//   bucket: {
-//     slug: process.env.COSMIC_BUCKET || 'chejsfinalblog',
-//     media_url: 'https://cosmicjs.com/uploads',
-//     read_key: process.env.COSMIC_READ_KEY || 'Ip4oXhZ8sFpyAnP8AyJx4ohSGikREWqhVX9Tu9TRPtUndAb5aZ',
-//     write_key: process.env.COSMIC_WRITE_KEY || 'Kr0cuoFeZa1nFcFc0KKRAwg872BrslCiVnXyuoJsDXlvKrc72M'
-//   },
-// }
-
 // config.js
 export default {
   site: {
     title: 'CheJSfinalBlog'
   },
   bucket: {
-    slug: process.env.COSMIC_BUCKET,
+    slug: process.env.COSMIC_BUCKET || 'chejsfinalblog',
     media_url: 'https://cosmicjs.com/uploads',
-    read_key: process.env.COSMIC_READ_KEY,
-    write_key: process.env.COSMIC_WRITE_KEY,
+    read_key: process.env.COSMIC_READ_KEY || 'WkvbJQs6z0nvA30OjVs25cUXRM0e8PcChkbuZYsn7uRh536LMF',
+    write_key: process.env.COSMIC_WRITE_KEY || 'Z8pbSrpAJAsRGDfiDBq1r2QWPWo9XYC5iwVqIvpTkLIBPHLwSg'
   },
 }
+
+// // config.js
+// export default {
+//   site: {
+//     title: 'CheJSfinalBlog'
+//   },
+//   bucket: {
+//     slug: process.env.COSMIC_BUCKET,
+//     media_url: 'https://cosmicjs.com/uploads',
+//     read_key: process.env.COSMIC_READ_KEY,
+//     write_key: process.env.COSMIC_WRITE_KEY,
+//   },
+// }
